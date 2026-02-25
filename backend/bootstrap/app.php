d3024fc1a98a8985e886538d79ad5b267e0e352f
yuanthio <yuanthiovirly26.9a.tik@gmail.com>
2026-02-25 22:40:57 +0700

    feat: Register PelayanMiddleware in bootstrap/app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'pelayan' => \App\Http\Middleware\PelayanMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
